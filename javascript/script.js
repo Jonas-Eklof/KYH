7b2604856127c11def6da9aa3545c0d0f42c43be
Jonas Eklöf <jonas_eklof89@hotmail.com>
2024-10-26 20:41:28 +0200

    --

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,19 @@
-console.log("Hello");
-console.log("World");
-console.log("!");
+let a = 1;
+let b = "1";
+
+if (a == b) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+let greeting = "Goodbye world!";
+{
+  greeting = "Hello World";
+}
+console.log(greeting);
+
+console.log(typeof (5 || 8));
+
+let name = prompt("What is your name?");
+console.log(name);
