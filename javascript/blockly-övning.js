a0b209920dd7c43c81d4e8e347592c247edccc93
Jonas Eklöf <jonas_eklof89@hotmail.com>
2024-10-30 10:31:48 +0100

    blockly-övning

---
+++ javascript/blockly-övning.js
@@ -0,0 +1,49 @@
+console.log("Jonas");
+console.log(1024);
+console.log(11 + 5);
+console.log(4 * 64);
+console.log(1 + 100 - 50);
+console.log(1 + 2 + 3 + 4);
+
+let variable1 = 16;
+console.log(variable1);
+
+let variable2 = 2 * 4;
+console.log(variable2);
+
+let variable3 = 10;
+variable3 = 100;
+variable3 = 50;
+console.log(variable3);
+
+let variable4 = 10;
+variable4 = variable4 + 1;
+console.log(variable4);
+
+let x1 = 10;
+let y1 = 20;
+x1 = y1 + 1;
+y1 = x1 + 1;
+console.log(x1);
+console.log(y1);
+
+let variable5 = 20;
+variable5 = variable5 + 1;
+console.log(variable5);
+
+let variable6 = 500;
+variable6 = variable6 - 100;
+console.log(variable6);
+
+let var1 = 3;
+let var2 = 4;
+let var3 = 5;
+console.log(var1 + var2 + var3);
+console.log(var1 * var2 * var3);
+
+let villkor = true;
+if (villkor == true) {
+  console.log("villkoret är sant");
+} else {
+  console.log("villkoret är falskt");
+}
