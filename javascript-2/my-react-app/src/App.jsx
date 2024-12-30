f3b05c25f2d0873fca35170c8a380a5864eec236
Jonas E <jonas_eklof89@hotmail.com>
2024-12-30 23:16:31 +0100

    Testar Vite

--- javascript-2/my-react-app/src/App.jsx
+++ javascript-2/my-react-app/src/App.jsx
@@ -17,13 +17,14 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h1>Hello fucker</h1>
+      <h1>Hello world</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and save to test HMR !!!!!!!!!!
         </p>
       </div>
       <p className="read-the-docs">
--- javascript-2/my-react-app/vite.config.js
+++ javascript-2/my-react-app/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    hmr: true, // Aktivera HMR explicit
+    watch: {
+      usePolling: true, // Tvingar filövervakning med polling
+    },
+  },
+});
