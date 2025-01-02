97d5de0fd6c0bcc8c2e6f4eff0d12f00ad65b0ed
Jonas Eklöf <jonas_eklof89@hotmail.com>
2025-01-02 02:39:56 +0100

    react components

---
+++ javascript-2/my-react-app/src/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header>
+      <div>
+        <h1>Logo Here</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ javascript-2/my-react-app/src/ProfileCard.jsx
@@ -0,0 +1,12 @@
+function ProfileCard() {
+  return (
+    <div className="profile-card">
+      <img src="https://picsum.photos/200/200" alt="Profilbild" />
+      <h2>Jonas E</h2>
+      <p>Student</p>
+      <button>Knapp</button>
+    </div>
+  );
+}
+
+export default ProfileCard;
