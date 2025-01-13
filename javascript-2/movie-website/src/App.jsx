769af15cb13b6d3090f2edb89bda6c64d4e8df6e
Jonas Eklöf <jonas_eklof89@hotmail.com>
2025-01-14 00:32:12 +0100

    movie app

--- javascript-2/movie-website/src/App.jsx
+++ javascript-2/movie-website/src/App.jsx
@@ -3,11 +3,12 @@ import MovieCard from "./components/MovieCard";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
 import { Route, Routes } from "react-router-dom";
+import { MovieProvider } from "./contexts/MovieContext";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
+    <MovieProvider>
       <Navbar />
       <div className="main-content">
         <Routes>
@@ -15,7 +16,7 @@ function App() {
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </div>
-    </>
+    </MovieProvider>
   );
 }
 
--- javascript-2/movie-website/src/components/MovieCard.jsx
+++ javascript-2/movie-website/src/components/MovieCard.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "../css/MovieCard.css";
+import { useMovieContext } from "../contexts/MovieContext";
 
 function MovieCard({ movie }) {
-  function onFavoriteClick() {
-    alert("clicked");
+  const { isFavorite, addToFavorites, removeFromFavorites } = useMovieContext();
+  const favorite = isFavorite(movie.id);
+
+  function onFavoriteClick(e) {
+    e.preventDefault();
+    if (favorite) removeFromFavorites(movie.id);
+    else addToFavorites(movie);
   }
 
   return (
@@ -15,7 +21,10 @@ function MovieCard({ movie }) {
         />
       </div>
       <div className="movie-overlay">
-        <button className="favorite-btn" onClick={onFavoriteClick}>
+        <button
+          className={`favorite-btn ${favorite ? "active" : ""}`}
+          onClick={onFavoriteClick}
+        >
           ♥
         </button>
       </div>
